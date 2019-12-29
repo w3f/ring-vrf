9dfa74b1636b4ab50d6a09b54dc6cc40a92f39f6
Wei Tang <hi@that.world>
2019-12-29 20:10:52 +0100

    Typed structures and other refactorings

--- src/generator.rs
+++ src/generator.rs
@@ -1,21 +1,19 @@
 use bellman::groth16::{generate_random_parameters, Parameters,};
 use bellman::SynthesisError;
-use pairing::bls12_381::Bls12;
-use zcash_primitives::jubjub::JubjubBls12;
+use zcash_primitives::jubjub::JubjubEngine;
 use rand_core::OsRng;
-use crate::Ring;
+use crate::{Ring, Params};
 
 /// Generates structured (meaning circuit-depending) Groth16
 /// CRS (that comprises proving and verificaton keys) over BLS12-381
 /// for the circuit defined in circuit.rs using OS RNG.
-pub fn generate_crs() -> Result<Parameters<Bls12>, SynthesisError> {
-    let params = &JubjubBls12::new();
+pub fn generate_crs<E: JubjubEngine>(params: &Params<E>) -> Result<Parameters<E>, SynthesisError> {
     let rng = &mut OsRng;
     let circuit = Ring {
         params,
         sk: None,
         vrf_input: None,
-        auth_path: vec![None; 10],
+        auth_path: None,
     };
     generate_random_parameters(circuit, rng)
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,10 +5,20 @@ mod prover;
 mod verifier;
 
 pub use crate::circuit::Ring;
-pub use crate::merkle::{aggregated_pkx, PathDirection};
+pub use crate::merkle::{MerkleSelection, AuthPath, AuthRoot, AuthPathPoint, auth_hash};
 pub use crate::generator::generate_crs;
 pub use crate::prover::prove;
 pub use crate::verifier::verify;
+
+use zcash_primitives::jubjub::JubjubEngine;
+
+/// Configuration parameters for the system.
+pub struct Params<E: JubjubEngine> {
+    /// Engine parameters.
+    pub engine: E::Params,
+    /// Authentication depth.
+    pub auth_depth: usize,
+}
 
 #[cfg(test)]
 mod tests {
@@ -18,7 +28,7 @@ mod tests {
     use ff::Field;
     use bellman::groth16::{prepare_verifying_key, Parameters};
     use zcash_primitives::jubjub::{JubjubBls12, JubjubParams, FixedGenerators, fs, edwards};
-    use pairing::bls12_381::{Bls12, Fr};
+    use pairing::bls12_381::Bls12;
     use rand_core::SeedableRng;
     use rand_xorshift::XorShiftRng;
 
@@ -26,19 +36,22 @@ mod tests {
 
     #[test]
     fn test_completeness() {
-        let rng = &mut XorShiftRng::from_seed([
-            0x58, 0x62, 0xbe, 0x3d, 0x76, 0x3d, 0x31, 0x8d, 0x17, 0xdb, 0x37, 0x32, 0x54, 0x06, 0xbc,
-            0xe5,
-        ]);
+        let params = Params::<Bls12> {
+            engine: JubjubBls12::new(),
+            auth_depth: 10,
+        };
 
-        let params = &JubjubBls12::new();
+        let rng = &mut XorShiftRng::from_seed([
+            0x58, 0x62, 0xbe, 0x3d, 0x76, 0x3d, 0x31, 0x8d,
+            0x17, 0xdb, 0x37, 0x32, 0x54, 0x06, 0xbc, 0xe5,
+        ]);
 
         let crs = match File::open("crs") {
             Ok(f) => Parameters::<Bls12>::read(f, false).expect("can't read CRS"),
             Err(_) => {
                 let f = File::create("crs").unwrap();
                 let t = SystemTime::now();
-                let c = generator::generate_crs().expect("can't generate CRS");
+                let c = generator::generate_crs(&params).expect("can't generate CRS");
                 println!("generation = {}", t.elapsed().unwrap().as_secs());
                 c.write(&f).unwrap();
                 c
@@ -46,32 +59,29 @@ mod tests {
         };
 
         // Jubjub generator point // TODO: prime or---
-        let base_point = params.generator(FixedGenerators::SpendingKeyGenerator);
+        let base_point = params.engine.generator(FixedGenerators::SpendingKeyGenerator);
 
         // validator's secret key, an element of Jubjub scalar field
         let sk = fs::Fs::random(rng);
 
         // validator's public key, a point on Jubjub
-        let pk = base_point.mul(sk, params);
+        let pk = base_point.mul(sk, &params.engine);
 
         // VRF base point
-        let vrf_base = edwards::Point::rand(rng, params).mul_by_cofactor(params);
+        let vrf_base = edwards::Point::rand(rng, &params.engine).mul_by_cofactor(&params.engine);
+        let vrf_output = vrf_base.mul(sk, &params.engine);
 
-        let vrf_output = vrf_base.mul(sk, params);
-
-        let tree_depth = 10;
-        let auth_path = vec![(Fr::random(rng), PathDirection::random(rng)); tree_depth];
-
-        let apkx = aggregated_pkx::<Bls12>(params, pk.to_xy().0, &auth_path);
+        let auth_path = AuthPath::random(params.auth_depth, rng);
+        let auth_root = AuthRoot::from_proof(&auth_path, &pk.to_xy().0, &params);
 
         let t = SystemTime::now();
-        let proof = prover::prove(params, &crs, sk, vrf_base.clone(), auth_path.clone());
+        let proof = prover::prove::<Bls12>(&crs, sk, vrf_base.clone(), auth_path.clone(), &params);
         println!("proving = {}", t.elapsed().unwrap().as_millis());
         let proof = proof.unwrap();
 
         let t = SystemTime::now();
         let pvk = prepare_verifying_key::<Bls12>(&crs.vk);
-        let valid = verifier::verify(&pvk, proof, vrf_base, vrf_output, apkx);
+        let valid = verifier::verify(&pvk, proof, vrf_base, vrf_output, auth_root);
         println!("verification = {}", t.elapsed().unwrap().as_millis());
         assert_eq!(valid.unwrap(), true);
     }
