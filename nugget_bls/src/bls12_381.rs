ba8cc3cbd7c7fe7831e7b7fdfa4e7aaf98325ab8
¨Jeff <burdges@gnunet.org>
2023-03-23 15:20:03 +0100

    Add convenience types

--- nugget_bls/src/bls12_381.rs
+++ nugget_bls/src/bls12_381.rs
@@ -7,9 +7,11 @@ use ark_ec::{
     bls12::Bls12Config,
 };
 
+use dleq_vrf::vrf::{VrfInput, IntoVrfInput};
 
 
-pub use ark_bls12_381::{Bls12_381, G1Affine, G1Projective};
+pub use ark_bls12_381::{Bls12_381};
+use ark_bls12_381::{G1Affine, G1Projective};
 
 type H2C = MapToCurveBasedHasher::<
 G1Projective,
@@ -21,4 +23,19 @@ pub fn hash_to_curve(domain: &[u8],message: &[u8]) -> Result<dleq_vrf::vrf::VrfI
     dleq_vrf::vrf::ark_hash_to_curve::<G1Affine,H2C>(domain,message)
 }
 
+pub struct Message<'a> {
+    pub domain: &'a [u8],
+    pub message: &'a [u8],
+}
 
+impl<'a> IntoVrfInput<G1Affine> for Message<'a> {
+    fn into_vrf_input(self) -> VrfInput<G1Affine> {
+        hash_to_curve(self.domain,self.message)
+        .expect("Hash-to-curve error, IRTF spec forbids messages longer than 2^16!")
+    }
+}
+
+pub type SecretKey = crate::SecretKey<Bls12_381>;
+pub type PublicKey = crate::PublicKey<Bls12_381>;
+pub type Signature = crate::Signature<Bls12_381>;
+pub type AggregateSignature = crate::AggregateSignature<Bls12_381>;
