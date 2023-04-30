0eada64e06e7f53316020218d4a16cd1bdd67a42
¨Jeff <burdges@gnunet.org>
2023-04-30 12:05:37 +0200

    Improve remote signer traits
    We now as the EcVrfSigner borrows the EcVrfSecret, but they'd always be
    identical for ThinVrf, and this makes sense for the bandersnatch ring VRF

--- dleq_vrf/src/traits.rs
+++ dleq_vrf/src/traits.rs
@@ -20,45 +20,144 @@
 //! TODO:  We have not evaluated if these traits mesh wqell with
 //! batch verification or threshold multi-signed VRFs.
 //! 
-//! TODO:  `IntoTranscript` does not correctly abstract signing for
+//! TODO:  `IntoTranscript` might not correctly abstract signing for
 //! a remote signer.
 
 
-pub use crate::vrf;
+pub use crate::vrf::{self, VrfInput, VrfPreOut, VrfInOut};
+
 
 type VrfResult<T> = Result<T,&'static str>
+
+
+/// VRF secret key.
+/// 
+/// Inherent methods and other traits being used here:
+/// `vrf::{IntoVrfInput, VrfInOut}`
+/// 
+/// We support multiple pre-output curves for the same secret key
+/// vai this formulation, which maybe overkill for polkadot, but
+/// makes some sense.
+pub trait EcVrfSecret<H: AffineRepr> {
+    /// 
+    pub fn vrf_preout<H>(&self, input: &VrfInput<H>) -> VrfPreOut<H>;
+
+    /// Create an `InputOutput` for usage both in signing as well as
+    /// in protocol buisness logic.
+    /// 
+    /// Always a thin wrapper around `SecretKey::vrf_inout` defined in
+    /// the `dleq_vrf::vrf`, but our secret key remains abstract here.
+    fn vrf_inout(&self, input: impl IntoVrfInput<H>) -> VrfInOut<H> {
+        let input = input.into_vrf_input();
+        let preoutput = self.vrf_preout(&input);
+        VrfInOut { input, preoutput }
+    }
+}
+
+/// TODO:  We should delegate the `SecretKey::{vrf_preout, vrf_inout}`
+/// in the `vrf` module to these ones, so they'll always agree.
+impl<H,K> EcVrfSecret<H> for SecretKey<K>
+where K: AffineRepr, H: AffineRepr<ScalarField = K::ScalarField>,
+{
+    fn vrf_preout<H>(&self, input: &vrf::VrfInput<H>) -> VrfPreOut<H> {
+        VrfPreOut( (&self.key * &input.0).into_affine() )
+    }
+
+    fn vrf_inout(&self, input: impl IntoVrfInput<H>) -> VrfInOut<H>;
+}
 
 
 /// VRF protocol parameters, flavor, etc. 
 pub trait EcVrf {
     /// Target group for hash-to-curve
     type H: AffineRepr;
-
     /// Detached signature aka proof type created by the VRF
     type VrfProof: Debug,Clone,PartialEq,Eq,Hash,CanonicalSerialize,CanonicalDeserialize;
 }
 
-type VrfInput<V> = crate::vrf::VrfInput<<V as EcVrf>::H>;
-type VrfPreOut<V> = crate::vrf::VrfPreOut<<V as EcVrf>::H>;
-type VrfInOut<V> = crate::vrf::VrfInOut<<V as EcVrf>::H>;
+pub type EcVrfInput<V> = VrfInput<<V as EcVrfIO>::H>;
+pub type EcVrfPreOut<V> = VrfPreOut<<V as EcVrfIO>::H>;
+pub type EcVrfInOut<V> = VrfInOut<<V as EcVrfIO>::H>;
 
 #[derive(Debug,Clone,PartialEq,Eq,Hash,CanonicalSerialize,CanonicalDeserialize)]
-pub struct VrfSignature<V: EcVrf, const N: usize> {
+pub struct VrfSignature<V: EcVrfProof, const N: usize> {
     pub proof: V::VrfProof,
     pub preouts: [VrfPreOut<V>; N],
 }
 
 #[derive(Debug,Clone,PartialEq,Eq,Hash,CanonicalSerialize,CanonicalDeserialize)]
-pub struct VrfSignatureVec<V: EcVrf> {
+pub struct VrfSignatureVec<V: EcVrfProof> {
     pub proof: V::VrfProof,
     pub preouts: Vec<VrfPreOut<V>>,
 }
+
+/// VRF signer, includes the secret key, but sometimes the ring opening too.
+/// 
+/// We do not provide pseduo-convenience methods like schnorrkel's
+/// `sign_extra_after_check`.  We've discovered too many VRF protocols
+/// need multiple input-output pairs, which makes convenience methods
+/// impossible.  instead, you should invoke `EcVrfSecret::vrf_inout`
+/// seperately for each input, run your buisness logic upon its output,
+/// and then pass whatever requires signing to `vrf_sign*`.  
+/// We do have convenience methods to handle multiple input groupings.
+/// 
+/// Inherent methods and other traits being used here:
+/// `IntoTranscript`, `vrf::{VrfInOut, VrfPreOut}`
+pub trait EcVrfSigner: EcVrf+Borrow<Secret> {
+    type Secret: EcVrfSecret<Self::H>;
+
+    fn vrf_sign_detached(
+        &self,
+        t: impl IntoTranscript,
+        ios: &[VrfInOut<Self>]
+    ) -> VrfResult<<Self as EcVrf>::VrfProof>;
+
+    fn vrf_sign<const N: usize>(
+        &self,
+        t: impl IntoTranscript,
+        ios: &[VrfInOut<Self>; N]
+    ) -> VrfResult<VrfSignature<Self,N>>
+    {
+        let proof = self.vrf_sign_detached(t,ios) ?;
+        let preouts = core::array::from_fn(|i| ios[i].preoutput.clone());
+        Ok(VrfSignature { preouts, proof })
+    }
+
+    fn vrf_sign_vec(
+        &self,
+        t: impl IntoTranscript,
+        ios: &[VrfInOut<Self>]
+    ) -> VrfResult<VrfSignatureVec<Self>>
+    {
+        let proof = self.vrf_sign_detached(t,ios) ?;
+        let preouts = ios.iter().map(|io| io.preoutput.clone()).collect();
+        Ok(VrfSignatureVec { preouts, proof })
+    }
+}
+
+impl<K: AffineRepr> EcVrf for SecretKey<K> {
+    type H = K;
+    type VrfProof = crate::Signature<crate::ThinVrf<K>>;
+}
+
+impl<K: AffineRepr> EcVrfSecret<K> for SecretKey<K> {
+    type Secret = Self;
+    fn vrf_sign_detached(
+        &self,
+        t: impl IntoTranscript,
+        ios: &[VrfInOut<Self>]
+    ) -> VrfResult<<Self as EcVrf>::VrfProof>
+    {
+        Ok(self.sign_thin_vrf(t,ios) ?)
+    }
+}
+
 
 /// VRF verifier, like a public key or ring commitment. 
 /// 
 /// Inherent methods and other traits being used here:
 /// `IntoTranscript`, `vrf::{IntoVrfInput, VrfPreOut::attach_input, VrfInOut}`
-pub trait EcVrfVerifier: EcVrf {
+pub trait EcVrfVerifier: EcVrfProof {
     fn vrf_verify_detached<'a>(
         &self,
         t: impl IntoTranscript,
@@ -96,64 +195,20 @@ pub trait EcVrfVerifier: EcVrf {
     }
 }
 
-/// VRF secret key.
-/// 
-/// Inherent methods and other traits being used here:
-/// `vrf::{IntoVrfInput, VrfInOut}`
-/// 
-/// We support multiple pre-output curves for the same secret key
-/// vai this formulation, which maybe overkill for polkadot, but
-/// makes some sense.
-pub trait EcVrfSecret<V: EcVrf> {
-    /// Create an `InputOutput` for usage both in signing as well as
-    /// in protocol buisness logic.
-    /// 
-    /// Always a thin wrapper around `SecretKey::vrf_inout` defined in
-    /// the `dleq_vrf::vrf`, but our secret key remains abstract here.
-    fn vrf_inout(&self, input: impl IntoVrfInput<<V as EcVrf>::H>) -> VrfInOut<V>;
+impl<K: AffineRepr> EcVrf<K> for (&ThinVrf<K>,&PublicKey<K>) {
+    type H = K;
+    type VrfProof = crate::Signature<crate::ThinVrf<K>>;
 }
 
-/// VRF signer, includes the secret key, but sometimes the ring opening too.
-/// 
-/// We do not provide pseduo-convenience methods like schnorrkel's
-/// `sign_extra_after_check`.  We've discovered too many VRF protocols
-/// need multiple input-output pairs, which makes convenience methods
-/// impossible.  instead, you should invoke `EcVrfSecret::vrf_inout`
-/// seperately for each input, run your buisness logic upon its output,
-/// and then pass whatever requires signing to `vrf_sign*`.  
-/// We do have convenience methods to handle multiple input groupings.
-/// 
-/// Inherent methods and other traits being used here:
-/// `IntoTranscript`, `vrf::{VrfInOut, VrfPreOut}`
-pub trait EcVrfSigner: EcVrf+Deref<Target=Secret> {
-    type Secret: EcVrfSecret<Self>;
-
-    fn vrf_sign_detached(
+impl<K: AffineRepr> EcVrfVerifier<K> for (&ThinVrf<K>,&PublicKey<K>) {
+    fn vrf_verify_detached<'a>(
         &self,
         t: impl IntoTranscript,
-        ios: &[VrfInOut<Self>]
-    ) -> VrfResult<<Self as EcVrf>::VrfProof>;
-
-    fn vrf_sign<const N: usize>(
-        &self,
-        t: impl IntoTranscript,
-        ios: &[VrfInOut<Self>; N]
-    ) -> VrfResult<VrfSignature<Self,N>>
+        ios: &'a [VrfInOut<Self>],        
+        signature: &<Self as EcVrf>VrfProof,
+    ) -> VrfResult<&'a [VrfInOut<K>]>
     {
-        let proof = self.vrf_sign_detached(t,ios) ?;
-        let preouts = core::array::from_fn(|i| ios[i].preoutput.clone());
-        Ok(VrfSignature { preouts, proof })
-    }
-
-    fn vrf_sign_vec(
-        &self,
-        t: impl IntoTranscript,
-        ios: &[VrfInOut<Self>]
-    ) -> VrfResult<VrfSignatureVec<Self>>
-    {
-        let proof = self.vrf_sign_detached(t,ios) ?;
-        let preouts = ios.iter().map(|io| io.preoutput.clone()).collect();
-        Ok(VrfSignatureVec { preouts, proof })
+        self.0.verify_thin_vrf(t,ios,self.1,signature)
     }
 }
 
