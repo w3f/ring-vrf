2e89f0aad3a5b9a03b115755db5c8002dc4ddc0f
Davide Galassi <davxy@datawok.net>
2023-09-26 10:33:46 +0200

    Bump ark-scale version to 0.0.11

--- dleq_vrf/src/scale.rs
+++ dleq_vrf/src/scale.rs
@@ -1,6 +1,6 @@
 
 use ark_std::vec::Vec;
-use ark_serialize::{CanonicalSerialize}; 
+use ark_serialize::{CanonicalSerialize, Compress}; 
 use ark_ec::AffineRepr;
 
 // #[macro_use]
@@ -23,31 +23,29 @@ use crate::{
 
 macro_rules! impl_point_wrapper {
     ($t:ident) => {
+        impl<C: AffineRepr> Decode for $t<C> {
+            impl_decode_via_ark!();
+        }
 
-impl<C: AffineRepr> Decode for $t<C> {
-    impl_decode_via_ark!();
-}
+        impl<C: AffineRepr> Encode for $t<C> {
+            impl_encode_via_ark!();
+        }
 
-impl<C: AffineRepr> Encode for $t<C> {
-    impl_encode_via_ark!();
-}
+        impl<C: AffineRepr> EncodeLike for $t<C> {}
 
-impl<C: AffineRepr> EncodeLike for $t<C> {}
+        impl<C: AffineRepr> MaxEncodedLen for $t<C> {
+            #[inline]
+            fn max_encoded_len() -> usize {
+                <C as AffineRepr>::zero().compressed_size()
+            }
+        }
 
-impl<C: AffineRepr> MaxEncodedLen for $t<C> {
-    #[inline]
-    fn max_encoded_len() -> usize {
-        <C as AffineRepr>::zero().compressed_size()
-    }
-}
-
-impl<C: AffineRepr> ArkScaleMaxEncodedLen for $t<C> {
-    #[inline]
-    fn max_encoded_len() -> usize {
-        <C as AffineRepr>::zero().compressed_size()
-    }
-}
-
+        impl<C: AffineRepr> ArkScaleMaxEncodedLen for $t<C> {
+            #[inline]
+            fn max_encoded_len(compress: Compress) -> usize {
+                <C as AffineRepr>::zero().serialized_size(compress)
+            }
+        }
     }
 } // macro_rules! impl_point_wrapper
 
@@ -80,22 +78,22 @@ impl<P: EcVrfProof, const N: usize> EncodeLike for VrfSignature<P,N> {}
 impl<P: EcVrfProof+ArkScaleMaxEncodedLen, const N: usize> MaxEncodedLen for VrfSignature<P,N> {
     fn max_encoded_len() -> usize {
         let o = <<P as EcVrfProof>::H as AffineRepr>::zero().compressed_size();
-        N * o + <P as ArkScaleMaxEncodedLen>::max_encoded_len()
+        N * o + <P as ArkScaleMaxEncodedLen>::max_encoded_len(Compress::Yes)
     }
 }
 
 impl<SF: ark_ff::PrimeField,const B: usize> ArkScaleMaxEncodedLen for crate::pedersen::Scalars<SF,B> {
-    fn max_encoded_len() -> usize {
-        B * <SF as ark_ff::Zero>::zero().compressed_size()
+    fn max_encoded_len(compress: Compress) -> usize {
+        B * <SF as ark_ff::Zero>::zero().serialized_size(compress)
     }
 }
 
 impl<K,H> ArkScaleMaxEncodedLen for crate::pedersen::Affines<K,H> 
 where K: AffineRepr, H: AffineRepr<ScalarField = K::ScalarField>,
 {
-    fn max_encoded_len() -> usize {
-        <K as AffineRepr>::zero().compressed_size()
-        + <H as AffineRepr>::zero().compressed_size()
+    fn max_encoded_len(compress: Compress) -> usize {
+        <K as AffineRepr>::zero().serialized_size(compress)
+        + <H as AffineRepr>::zero().serialized_size(compress)
     }
 }
 
@@ -106,11 +104,11 @@ where
     // <F as InnerFlavor>::Scalars: ArkScaleMaxEncodedLen,
     <F as InnerFlavor>::Affines: ArkScaleMaxEncodedLen,
 {
-    fn max_encoded_len() -> usize {
-        <<F as InnerFlavor>::KeyCommitment as ArkScaleMaxEncodedLen>::max_encoded_len()
-        + <F as InnerFlavor>::Scalars::default().compressed_size()
+    fn max_encoded_len(compress: Compress) -> usize {
+        <<F as InnerFlavor>::KeyCommitment as ArkScaleMaxEncodedLen>::max_encoded_len(compress)
+        + <F as InnerFlavor>::Scalars::default().serialized_size(compress)
         // + <<F as InnerFlavor>::Scalars as ArkScaleMaxEncodedLen>::max_encoded_len()
-        + <<F as InnerFlavor>::Affines as ArkScaleMaxEncodedLen>::max_encoded_len()
+        + <<F as InnerFlavor>::Affines as ArkScaleMaxEncodedLen>::max_encoded_len(compress)
     }
 }
 
@@ -119,13 +117,10 @@ where
     <F as InnerFlavor>::KeyCommitment: ArkScaleMaxEncodedLen,
     // <F as InnerFlavor>::Scalars: ArkScaleMaxEncodedLen,
 {
-    fn max_encoded_len() -> usize {
-        <<F as InnerFlavor>::KeyCommitment as ArkScaleMaxEncodedLen>::max_encoded_len()
-        + <F as InnerFlavor>::Scalars::default().compressed_size()
+    fn max_encoded_len(compress: Compress) -> usize {
+        <<F as InnerFlavor>::KeyCommitment as ArkScaleMaxEncodedLen>::max_encoded_len(compress)
+        + <F as InnerFlavor>::Scalars::default().serialized_size(compress)
         // + <<F as InnerFlavor>::Scalars as ArkScaleMaxEncodedLen>::max_encoded_len()
-        + <<F as Flavor>::ScalarField as ark_ff::Zero>::zero().compressed_size()
+        + <<F as Flavor>::ScalarField as ark_ff::Zero>::zero().serialized_size(compress)
     }
 }
-
-
-
